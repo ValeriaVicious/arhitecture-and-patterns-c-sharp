a99599af9feb4aff070725845837d07207668844
ValeriaVicious <valeriavicious13@gmail.com>
2021-07-13 14:41:40 +0200

    описан класс ввода пользователя по горизонтали

--- patterns-arhitecture/Assets/Code/Input/InputFire.cs
+++ patterns-arhitecture/Assets/Code/Input/InputFire.cs
@@ -6,12 +6,21 @@ namespace MonkeyInTheSpace.GeekBrains
 {
     internal sealed class InputFire : IUserFireProxy
     {
+        #region Fields
+
         public event Action<bool> FireInputGetDown =
             delegate (bool isFireGetDown) { };
+
+        #endregion
+
+
+        #region Methods
 
         public void GetFireDown()
         {
             FireInputGetDown.Invoke(Input.GetButtonDown(Constants.FireInput));
         }
+
+        #endregion
     }
 }
