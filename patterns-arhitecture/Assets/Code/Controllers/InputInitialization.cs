021f18b442f533aff340f9061f3f266c55635378
ValeriaVicious <valeriavicious13@gmail.com>
2021-07-13 15:11:54 +0200

    описан класс InputInitialization,в котором создаются объекты инпута горизонтали, вертикали, стрельбы

---
+++ patterns-arhitecture/Assets/Code/Controllers/InputInitialization.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace MonkeyInTheSpace.GeekBrains
+{
+    internal sealed class InputInitialization : IInitialization
+    {
+        #region Fields
+
+        private IUserInputProxy _verticalInput;
+        private IUserInputProxy _horizontalInput;
+        private IUserFireProxy _fireInput;
+
+        #endregion
+
+
+        #region ClassLifeCycles
+
+        public InputInitialization()
+        {
+            _horizontalInput = new InputHorizontal();
+            _fireInput = new InputFire();
+            _verticalInput = new InputVertical();
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public void Initiallization()
+        {
+
+        }
+
+        public (IUserInputProxy inputHorizontal, IUserInputProxy inputVertical) GetInput()
+        {
+            (IUserInputProxy inputHorizontal, IUserInputProxy inputVertical) result = (_horizontalInput, _verticalInput);
+            return result;
+        }
+
+        public IUserFireProxy GetFire()
+        {
+            return _fireInput;
+        }
+
+        #endregion
+    }
+}
