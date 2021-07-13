93a9b6934d696bac1653eafa7fddfa266b18a0bc
ValeriaVicious <valeriavicious13@gmail.com>
2021-07-13 14:38:47 +0200

    Описан класс стрельбы вводы пользователя

---
+++ patterns-arhitecture/Assets/Code/Input/InputFire.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace MonkeyInTheSpace.GeekBrains
+{
+    internal sealed class InputFire : IUserFireProxy
+    {
+        public event Action<bool> FireInputGetDown =
+            delegate (bool isFireGetDown) { };
+
+        public void GetFireDown()
+        {
+            FireInputGetDown.Invoke(Input.GetButtonDown(Constants.FireInput));
+        }
+    }
+}
