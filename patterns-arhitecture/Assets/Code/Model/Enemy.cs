20aec81576a3f147be8b62fc67bc7a767020eb8f
ValeriaVicious <valeriavicious13@gmail.com>
2021-08-03 14:16:18 +0200

    - Рефакторинг класса Enemy - Создание отдельного класса, который отвечает за контроль полет врагов за за границы экрана

--- patterns-arhitecture/Assets/Code/Model/Enemy.cs
+++ patterns-arhitecture/Assets/Code/Model/Enemy.cs
@@ -8,9 +8,6 @@ namespace MonkeyInTheSpace.GeekBrains
     {
         #region Fields
 
-        private EnemyConfig _enemyData;
-        private static float _theEndPointOfTheEnemyFall = 10.0f;
-
         public Action<GameObject> OnEnemyOverFly;
         public Action<GameObject> OnTriggerEnterChanging;
 
@@ -18,41 +15,9 @@ namespace MonkeyInTheSpace.GeekBrains
 
         #region Properties
 
-        public int Damage => _enemyData.EnemyDamage;
-        public static float EndPointOfTheEnemyFall => _theEndPointOfTheEnemyFall;
-
         public Transform GetTransform => transform;
 
         #endregion
-
-
-        #region UnityMethods
-
-        private void FixedUpdate()
-        {
-            TheEnemyFlewOffTheScreen();
-        }
-
-        #endregion
-
-
-        #region Methods
-
-        private void TheEnemyFlewOffTheScreen()
-        {
-            transform.Translate(Vector3.down * _enemyData.Speed);
-            if (transform.position.y < -_theEndPointOfTheEnemyFall && OnEnemyOverFly != null)
-            {
-                OnEnemyOverFly(gameObject);
-            }
-        }
-
-        public void InitOfEnemy(EnemyConfig enemyData)
-        {
-            _enemyData = enemyData;
-            GetComponent<SpriteRenderer>().sprite = _enemyData.MainSprite;
-        }
-
-        #endregion
+ 
     }
 }
---
+++ patterns-arhitecture/Assets/Code/Model/InitAndFlewTheEnemies.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+
+namespace MonkeyInTheSpace.GeekBrains
+{
+    internal sealed class InitAndFlewTheEnemies : IFixedExecute
+    {
+        #region Fields
+
+        private Enemy _enemy;
+        private EnemyConfig _enemyData;
+        private float _theEndPointOfTheEnemyFall = 10.0f;
+
+        #endregion
+
+
+        #region Methods
+
+        public void FixedExecute(float deltaTime)
+        {
+            TheEnemyFlewOffTheScreen();
+        }
+
+        private void TheEnemyFlewOffTheScreen()
+        {
+            _enemy.GetTransform.Translate(Vector3.down * _enemyData.Speed);
+            if (_enemy.transform.position.y < -_theEndPointOfTheEnemyFall && _enemy.OnEnemyOverFly != null)
+            {
+                _enemy.OnEnemyOverFly(_enemy.gameObject);
+            }
+        }
+
+        public void InitOfEnemy(EnemyConfig enemyData)
+        {
+            _enemyData = enemyData;
+            _enemy.GetComponent<SpriteRenderer>().sprite = _enemyData.MainSprite;
+        }
+
+        #endregion
+
+
+    }
+}
