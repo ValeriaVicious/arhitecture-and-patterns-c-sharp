9852441774615cc231a5c586ffb67008e193e5a7
ValeriaVicious <valeriavicious13@gmail.com>
2021-08-11 11:21:16 +0200

    Реализация примера паттерна Bridge

---
+++ patterns-arhitecture/Assets/Code/StructuralPatterns/Bridge/ExampleBridge/Example.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+namespace MonkeyInTheSpace.GeekBrains.StructuralPatterns.Bridge
+{
+    internal sealed class Example : MonoBehaviour
+    {
+        private void Start()
+        {
+            var enemy = new Enemy(new MagicalAttack(), new Infantry());
+        }
+    }
+}
---
+++ patterns-arhitecture/Assets/Code/StructuralPatterns/Bridge/Logic/Infantry.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace MonkeyInTheSpace.GeekBrains.StructuralPatterns.Bridge
+{
+    internal sealed class Infantry : IMove
+    {
+        public void Move()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
