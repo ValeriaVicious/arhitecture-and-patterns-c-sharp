1c4303f1b2828b8fdf44f9f8c72b473d85228002
ValeriaVicious <valeriavicious13@gmail.com>
2021-08-16 12:09:33 +0200

    Описание контроллера движения врага

---
+++ patterns-arhitecture/Assets/Code/Interface/IMoveEnemy.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace MonkeyInTheSpace.GeekBrains
+{
+    public interface IMoveEnemy : IController
+    {
+        public void Move(float horizontal, float vertical, float deltaTime);
+    }
+}
