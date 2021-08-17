2444c81f4da8b5761e6fcd4fdc4264202e094d3e
ValeriaVicious <valeriavicious13@gmail.com>
2021-08-17 16:27:50 +0200

    Создание интерпретатора для вывода чисел в нужном формате

---
+++ patterns-arhitecture/Assets/Code/Interpreter/InterpreterNumbersForScoreUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+
+namespace MonkeyInTheSpace.GeekBrains
+{
+    public static class InterpreterNumbersForScoreUI
+    {
+        #region Fields
+
+        private static string[] _namesOfNumbers = { "", "K", "M", "B", "T" };
+        private static string _zero = "0";
+        private static double _maxDoubleNumber = 1000.0d;
+
+        #endregion
+
+
+        #region Methods
+
+        public static string Format(double number)
+        {
+            number = Mathf.Round((float)number);
+            var i = 0;
+
+            if (number == 0)
+            {
+                return _zero;
+            }
+
+            while (i + 1 < _namesOfNumbers.Length && number >= _maxDoubleNumber)
+            {
+                number /= _maxDoubleNumber;
+                i++;
+            }
+            return $"{number:#.##}{_namesOfNumbers[i]}";
+        }
+
+        #endregion
+    }
+}
---
+++ patterns-arhitecture/Assets/Code/OnDisplay/TextScoreOnDisplay.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace MonkeyInTheSpace.GeekBrains
+{
+    public sealed class TextScoreOnDisplay
+    {
+        #region Fields
+
+        private Text _text;
+
+        #endregion
+
+
+        #region ClassLifeCycles
+
+        public TextScoreOnDisplay(GameObject textScoreObject)
+        {
+            _text = textScoreObject.GetComponentInChildren<Text>();
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public void Display(int score)
+        {
+            _text.text = InterpreterNumbersForScoreUI.Format(score);
+        }
+
+        #endregion
+    }
+}
