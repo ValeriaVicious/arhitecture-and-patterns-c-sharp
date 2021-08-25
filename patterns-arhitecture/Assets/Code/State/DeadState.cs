9a32e76be36cfcd9a504c6351cefcb778402b4a0
ValeriaVicious <valeriavicious13@gmail.com>
2021-08-25 14:25:52 +0200

    Простая реализация паттерна State

---
+++ patterns-arhitecture/Assets/Code/State/DeadState.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace MonkeyInTheSpace.GeekBrains
+{
+    public sealed class DeadState : State
+    {
+        public override void Handle(CharacterMovementState characterMovementType)
+        {
+            characterMovementType.State = new MoveState();
+        }
+    }
+}
---
+++ patterns-arhitecture/Assets/Code/State/StateExample.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+namespace MonkeyInTheSpace.GeekBrains
+{
+    public sealed class StateExample : MonoBehaviour
+    {
+        private void Start()
+        {
+            CharacterMovementState characterMovementState = new CharacterMovementState(new MoveState());
+            characterMovementState.Request();
+            characterMovementState.Request();
+            characterMovementState.Request();
+            characterMovementState.Request();
+        }
+    }
+}
